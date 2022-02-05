355ab859f898f55f486d39e037f19225d3f122f7
Harry Prior <harryprior@hotmail.co.uk>
2022-02-05 10:59:19 +0000

    add type annotations

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,9 +1,23 @@
 module PhotoGroove exposing (main)
 
+-- import Array exposing (Array)
+
 import Browser exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+
+
+type alias PhotoUrl =
+    { url : String }
+
+
+type alias Msg =
+    { description : String, data : String }
+
+
+type alias Model =
+    { photos : List PhotoUrl, selectedUrl : String }
 
 
 urlPrefix : String
@@ -11,6 +25,7 @@ urlPrefix =
     "http://elm-in-action.com/"
 
 
+getThumbnailUrls : String -> PhotoUrl -> Html Msg
 getThumbnailUrls selectedUrl thumb =
     img
         [ src (urlPrefix ++ thumb.url)
@@ -20,13 +35,14 @@ getThumbnailUrls selectedUrl thumb =
         []
 
 
+view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "PhotoGroove" ]
         , div [ id "thumbnails" ]
             (model.photos |> List.map (getThumbnailUrls model.selectedUrl))
 
-        -- returns a list of images which are the children of the div 
+        -- returns a list of images which are the children of the div
         , img
             [ class "large"
             , src (urlPrefix ++ "large/" ++ model.selectedUrl)
@@ -35,11 +51,17 @@ view model =
         ]
 
 
-initModel : { photos : List { url : String }, selectedUrl : String }
+initModel : { photos : List PhotoUrl, selectedUrl : String }
 initModel =
     { photos = [ { url = "1.jpeg" }, { url = "2.jpeg" }, { url = "3.jpeg" }, { url = "4.jpeg" } ] -- used to display images
     , selectedUrl = "1.jpeg" -- used to display larger image
     }
+
+
+
+-- photoArray : Array PhotoUrl
+-- photoArray =
+--     Array.fromList initModel.photos
 
 
 update msg model =
